fd9ac951d286d91766d28f117b31357e5b2e34be
mocitheccat <mocitheccat@gmail.com>
2024-06-03 20:59:49 +0200

    another more image path fix

--- src/Components/AccountMenu.jsx
+++ src/Components/AccountMenu.jsx
@@ -1,4 +1,5 @@
 import Auth from "../utils/auth.js";
+import defaultBlueImage from "../../public/images/default-blue.png";
 
 const AccountMenu = ({ visible }) => {
   const username = Auth.readUser();
@@ -16,11 +17,7 @@ const AccountMenu = ({ visible }) => {
     <div className="bg-black w-56 absolute top-14 right-0 py-5 flex-col border-2 border-gray-800 flex">
       <div className="flex flex-col gap-3">
         <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
-          <img
-            className="w-8 rounded-md"
-            src="public/images/default-blue.png"
-            alt=""
-          />
+          <img className="w-8 rounded-md" src={defaultBlueImage} alt="" />
           <p className="text-white text-sm group-hover/item:underline">
             {username}
           </p>
@@ -28,7 +25,7 @@ const AccountMenu = ({ visible }) => {
       </div>
       <hr className="bg-gray-600 border-0 h-px my-4" />
       <div
-        onClick={() => handleLogout()}
+        onClick={handleLogout}
         className="px-3 text-center text-white text-sm hover:underline"
       >
         Sign out of Mooovix
