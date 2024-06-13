518cfeb00d8b7bda31ec74e34b11aa1a13936462
mocitheccat <163505159+mocitheccat@users.noreply.github.com>
2024-06-13 00:06:51 +0000

    Replit commit to fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,38 @@
+// import { Route, Routes } from "react-router-dom";
+// import LoginPage from "./Routes/LoginPage.jsx";
+// import HomePage from "./Routes/HomePage.jsx";
+// import ProtectedRoute from "./Routes/Protected/ProtectedRoute";
+// import { TMDBProvider } from "./Context/ApiContext";
+// import SearchPage from "./Routes/SearchPage.jsx";
+// import Layout from "./Components/Layout.jsx";
+// import MediaDetailPage from "./Routes/MediaDetailPage.jsx";
+
+// function App() {
+//   return (
+//     <TMDBProvider>
+//       <Layout>
+//         <Routes>
+//           {/* Public Routes */}
+//           <Route element={<ProtectedRoute requiresAuth={false} />}>
+//             <Route path="/login" element={<LoginPage />} />
+//           </Route>
+
+//           {/* Private Routes with Layout */}
+//           <Route element={<ProtectedRoute requiresAuth={true} />}>
+//             <Route path="/" element={<HomePage />} />
+//             <Route path="/search" element={<SearchPage />} />
+//             <Route path="/movie/:id" element={<MediaDetailPage />} />
+//             <Route path="/tv/:id" element={<MediaDetailPage />} />
+//           </Route>
+//         </Routes>
+//       </Layout>
+//     </TMDBProvider>
+//   );
+// }
+
+// export default App;
+
+
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "./Routes/LoginPage.jsx";
 import HomePage from "./Routes/HomePage.jsx";
@@ -13,17 +48,41 @@ function App() {
       <Layout>
         <Routes>
           {/* Public Routes */}
-          <Route element={<ProtectedRoute requiresAuth={false} />}>
-            <Route path="/login" element={<LoginPage />} />
-          </Route>
+          <Route path="/login" element={<LoginPage />} />
 
           {/* Private Routes with Layout */}
-          <Route element={<ProtectedRoute requiresAuth={true} />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/search" element={<SearchPage />} />
-            <Route path="/movie/:id" element={<MediaDetailPage />} />
-            <Route path="/tv/:id" element={<MediaDetailPage />} />
-          </Route>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <HomePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/search"
+            element={
+              <ProtectedRoute>
+                <SearchPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/movie/:id"
+            element={
+              <ProtectedRoute>
+                <MediaDetailPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/tv/:id"
+            element={
+              <ProtectedRoute>
+                <MediaDetailPage />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </Layout>
     </TMDBProvider>
--- src/Routes/Protected/ProtectedRoute.jsx
+++ src/Routes/Protected/ProtectedRoute.jsx
@@ -1,17 +1,32 @@
-import { Navigate, Outlet } from "react-router-dom";
+// import { Navigate, Outlet } from "react-router-dom";
 
-const ProtectedRoute = ({ requiresAuth }) => {
+// const ProtectedRoute = ({ requiresAuth }) => {
+//   const username = localStorage.getItem("username");
+
+//   if (requiresAuth && !username) {
+//     return <Navigate to="/login" />;
+//   }
+
+//   if (!requiresAuth && username) {
+//     return <Navigate to="/" />;
+//   }
+
+//   return <Outlet />;
+// };
+
+// export default ProtectedRoute;
+
+
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
   const username = localStorage.getItem("username");
 
-  if (requiresAuth && !username) {
+  if (!username) {
     return <Navigate to="/login" />;
   }
 
-  if (!requiresAuth && username) {
-    return <Navigate to="/" />;
-  }
-
-  return <Outlet />;
+  return children;
 };
 
 export default ProtectedRoute;
